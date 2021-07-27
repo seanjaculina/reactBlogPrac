4f4f1aea0027fb1e5c584e5afea3a7039044f20f
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-07-26 19:15:22 -0700

    added property to Comment component, used props to pass and receive properties

---
+++ proj1/src/Comment.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import faker from "faker";
+
+const Comment = (props) =>{
+        return(
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {props.name}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">
+                            {faker.time.recent}
+                        </span>
+                    </div>
+                    <div className="text">
+                        Nice Blog Post
+                    </div>
+                </div>
+            </div>
+        )
+
+
+}
+
+export default Comment;
--- proj1/src/index.js
+++ proj1/src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Comment from './Comment';
+
 
 const App = () =>{
     return(
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sean
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today at 6:22PM
-                        </span>
-                    </div>
-                    <div className="text">
-                        Nice Blog Post
-
-                    </div>
-                </div>
-            </div>
+           <Comment name="Sean"/>
+           <Comment name="Jaz"/>
+           <Comment name="Kobe"/>
         </div>
     )
 }
