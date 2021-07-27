b47537b0989c905149c676a94eb21188435aa21e
Sean Jaculina <sjaculina@horizon.csueastbay.edu>
2021-07-26 18:20:36 -0700

    adding container for comments

--- proj1/src/index.js
+++ proj1/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () =>{
-    return <div>Hello!</div>
+    return(
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+            </div>
+        </div>
+    )
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
